54876bcbf56a2a128ecb93ed162e57e3cc883503
DanilGaripov <garipovdr2005@gmail.com>
2024-06-06 23:44:44 +0500

    lesson - 14

---
+++ 14/09.py
@@ -0,0 +1,12 @@
+def ask_password():
+    for i in range(0, 3):
+        s = input()
+        if s == 'password':
+            print('Пароль принят.')
+            break
+        elif i == 2:
+            print('В доступе отказано')
+    return
+
+
+ask_password()
