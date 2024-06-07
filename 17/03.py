701d2cdce743f3fe30473a7bb3f5bf6dd0b73fd3
DanilGaripov <garipovdr2005@gmail.com>
2024-06-07 18:09:58 +0500

    lesson - 17

---
+++ 17/03.py
@@ -0,0 +1,10 @@
+original_list = [3, 1, 4, 2]
+original_list.sort()
+
+print("Сортировка с помощью метода sort():", original_list)
+
+original_list = [3, 1, 4, 2]
+sorted_list = sorted(original_list)
+
+print("Сортировка с помощью функции sorted():", sorted_list)
+print("Исходный список остался неизменным:", original_list)
